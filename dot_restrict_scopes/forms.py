4cee4dec80ed7e19ca4ac1907947b21bb20275c0
Matt Pryor <matt.pryor@stfc.ac.uk>
2017-09-04 18:32:26 +0100

    Fix restricted application form

--- dot_restrict_scopes/forms.py
+++ dot_restrict_scopes/forms.py
@@ -19,9 +19,11 @@ class DelimitedListField(forms.MultipleChoiceField):
         self.delimiter = delimiter or self.delimiter
 
     def prepare_value(self, value):
-        # Prepares the value by splitting on the delimiter, trimming leading and
-        # trailing whitespace and excluding empty values
-        return [p.trim() for p in value.split(self.delimiter) if p.trim()]
+        # If the value is already a list or tuple, just use it as-is
+        if isinstance(value, (list, tuple)): return value
+        # Otherwise, prepare the value by splitting on the delimiter, trimming
+        # leading and trailing whitespace and excluding empty values
+        return [p.strip() for p in value.split(self.delimiter) if p.strip()]
 
     def clean(self, value):
         # Let the parent clean the value first, then join the result using the
@@ -33,15 +35,17 @@ class RestrictedApplicationForm(forms.ModelForm):
     """
     Form for creating or updating a restricted application.
     """
+    # allowed_scope is a space-delimited list, but we want to present
+    # a selection of valid scopes with checkboxes
+    allowed_scope = DelimitedListField(
+        label = 'Allowed scopes',
+        # The choices and initial values are callables, because the scopes might
+        # not be available at import type, e.g. if coming from the database
+        choices = lambda: get_scopes_backend().get_all_scopes().items(),
+        initial = lambda: get_scopes_backend().get_default_scopes(),
+        delimiter = ' ',
+        widget = forms.CheckboxSelectMultiple
+    )
+
     class Meta:
         exclude = ()
-        field_classes = {
-            # allowed_scope is a space-delimited list, but we want to present
-            # a selection of valid scopes with checkboxes
-            'allowed_scope': DelimitedListField(
-                choices = get_scopes_backend().get_all_scopes().items(),
-                initial = get_scopes_backend().get_default_scopes(),
-                delimiter = ' ',
-                widget = forms.CheckboxSelectMultiple
-            )
-        }
